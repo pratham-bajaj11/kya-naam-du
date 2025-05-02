675e0f42f74a019b34a3e5878912e4f1c3de6f3b
bajaj_pratham <prathambajaj0001@gmail.com>
2025-05-02 17:04:45 +0530

    firt commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    
+      <div>
+        <h1>My react app</h1>
+        <h2>My react implemtation</h2>
+        <h3>Pratham Bajaj</h3>
+       
+      </div>
+     
+    
+  )
+}
+
+export default App
